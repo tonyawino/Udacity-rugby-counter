bd173130760c84d8ada64a892da6326b22f06fff
ynotawino <ynotomondi57@gmail.com>
2018-08-04 01:09:04 +0300

    Add images with different resolutions

--- app/src/main/java/com/example/android/rugbycounter/MainActivity.java
+++ app/src/main/java/com/example/android/rugbycounter/MainActivity.java
@@ -7,8 +7,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int uganda_score=0;
-    int kenya_score=0;
+    private int uganda_score = 0;
+    private int kenya_score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,43 +17,43 @@ public class MainActivity extends AppCompatActivity {
         reset(new View(this));
     }
 
-    public void uganda_try(View view){
+    public void uganda_try(View view) {
         add_points_uganda(5);
     }
 
-    public void uganda_conversion(View view){
+    public void uganda_conversion(View view) {
         add_points_uganda(2);
     }
 
-    public void uganda_drop(View view){
+    public void uganda_drop(View view) {
         add_points_uganda(3);
     }
 
-    public void uganda_penalty(View view){
+    public void uganda_penalty(View view) {
         add_points_uganda(3);
     }
 
-    public void uganda_penalty_try(View view){
+    public void uganda_penalty_try(View view) {
         add_points_uganda(7);
     }
 
-    public void kenya_try(View view){
+    public void kenya_try(View view) {
         add_points_kenya(5);
     }
 
-    public void kenya_conversion(View view){
+    public void kenya_conversion(View view) {
         add_points_kenya(2);
     }
 
-    public void kenya_drop(View view){
+    public void kenya_drop(View view) {
         add_points_kenya(3);
     }
 
-    public void kenya_penalty(View view){
+    public void kenya_penalty(View view) {
         add_points_kenya(3);
     }
 
-    public void kenya_penalty_try(View view){
+    public void kenya_penalty_try(View view) {
         add_points_kenya(7);
 
     }
@@ -61,29 +61,29 @@ public class MainActivity extends AppCompatActivity {
     /*
         This method adds points to Kenya's score and displays it
      */
-    private void add_points_kenya(int points){
-        kenya_score+=points;
-        TextView textBox=findViewById(R.id.kenyaScore);
+    private void add_points_kenya(int points) {
+        kenya_score += points;
+        TextView textBox = findViewById(R.id.kenyaScore);
         textBox.setText(getString(R.string.score_kenya, kenya_score));
     }
 
     /*
         This method adds points to Uganda's score and displays it
      */
-    private void add_points_uganda(int points){
-        uganda_score+=points;
-        TextView textBox=findViewById(R.id.ugandaScore);
+    private void add_points_uganda(int points) {
+        uganda_score += points;
+        TextView textBox = findViewById(R.id.ugandaScore);
         textBox.setText(getString(R.string.score_uganda, uganda_score));
     }
 
     /*
         This method resets the scores to zero and displays them
      */
-    public void reset(View view){
-        TextView kenyaScoreView=findViewById(R.id.kenyaScore);
-        TextView ugandaScoreView=findViewById(R.id.ugandaScore);
-        kenya_score=0;
-        uganda_score=0;
+    public void reset(View view) {
+        TextView kenyaScoreView = findViewById(R.id.kenyaScore);
+        TextView ugandaScoreView = findViewById(R.id.ugandaScore);
+        kenya_score = 0;
+        uganda_score = 0;
         kenyaScoreView.setText(getString(R.string.score_kenya, kenya_score));
         ugandaScoreView.setText(getString(R.string.score_uganda, uganda_score));
 
